9fd71f98117ec353ccd22fadba4936865f060743
魂狩 <a364717646@163.com>
2016-11-27 00:31:11 +0800

    finish confirm

--- confirm/confirm.js
+++ confirm/confirm.js
@@ -1,57 +1,93 @@
-CONFIRM=(function(){
-    var tpl=null;
-    var src=(function(){
-        a = {},
-		expose = +new Date(),
-		rExtractUri = /((?:http|https|file):\/\/.*?\/[^:]+)(?::\d+)?:\d+/,
-		isLtIE8 = ('' + document.querySelector).indexOf('[native code]') === -1;
+CONFIRM = (function () {
+    var tpl = null;
+    var path = (function () {
+        var a = {},expose = +new Date(),
+            rExtractUri = /((?:http|https|file):\/\/.*?\/[^:]+)(?::\d+)?:\d+/,
+            isLtIE8 = ('' + document.querySelector).indexOf('[native code]') === -1;
         // FF,Chrome
-        if (document.currentScript){
+        if (document.currentScript) {
             return document.currentScript.src;
         }
 
         var stack;
-        try{
+        try {
             a.b();
         }
-        catch(e){
+        catch (e) {
             stack = e.fileName || e.sourceURL || e.stack || e.stacktrace;
         }
         // IE10
-        if (stack){
+        if (stack) {
             var absPath = rExtractUri.exec(stack)[1];
-            if (absPath){
+            if (absPath) {
                 return absPath;
             }
         }
         // IE5-9
-        for(var scripts = doc.scripts,
-            i = scripts.length - 1,
-            script; script = scripts[i--];){
-            if (script.className !== expose && script.readyState === 'interactive'){
+        for (var scripts = doc.scripts,
+                 i = scripts.length - 1,
+                 script; script = scripts[i--];) {
+            if (script.className !== expose && script.readyState === 'interactive') {
                 script.className = expose;
                 // if less than ie 8, must get abs path by getAttribute(src, 4)
                 return isLtIE8 ? script.getAttribute('src', 4) : script.src;
             }
         }
     })();
+    path=path.slice(0,-10);
     return {
-        show:function(message,fun){
-            if (tpl==null){
-                src=src.slice(0,-2)+'tpl';
-                $.get(src,'',function(data){
-                    tpl=$(data);
+        show: function (message, fun) {
+            var self = this;
+            if (tpl == null) {
+                $.get(path+'confirm.tpl', '', function (data) {
+                    tpl = $(data);
+                    tpl.find('.confirm-alert').css('background','url("'+path+'alert.png") no-repeat scroll 0 0');
                     tpl.appendTo(document.body);
-                    CONFIRM.show(message,fun);
+                    $('.confirm-mask,.confirm-close,.confirm-dismiss').on('click',function(e){
+                        self.closeWindow();
+                    });
+                    self.fixPosition($('.sys-div-confirm'));
+                    self.show(message, fun);
                 });
-            }else{
+            } else {
                 tpl.find('#confirm-message').html(message);
-                tpl.find('#confirm-btn').off('click').on('click',function(){
+                tpl.find('#confirm-btn').off('click').on('click', function () {
                     fun();
-                    tpl.hide();
+                    self.closeWindow();
                 });
-                tpl.show();
+                $('.confirm-mask').addClass('confirm-show')
+                tpl.fadeIn(300);
             }
+        },
+        closeWindow:function(){
+            $('.confirm-mask').removeClass('confirm-show').fadeOut(300);
+            tpl.fadeOut(300);
+        },
+        fixPosition: function (obj) { //居中显示
+            var screenWidth = $(window).width(),
+                screenHeight = $(window).height(),
+                scrolltop = $(document).scrollTop(); //当前浏览器窗口的宽和高
+            var objLeft = (screenWidth - obj.width()) / 2;
+            var objTop = (screenHeight - obj.height()) / 2 + scrolltop;
+            obj.css({left: objLeft + 'px', top: objTop + 'px'});
+
+            $(window).resize(function () { //窗口缩放时改变位置
+                screenWidth = $(window).width();
+                screenHeight = $(window).height();
+                scrolltop = $(document).scrollTop();
+                objLeft = (screenWidth - obj.width()) / 2;
+                objTop = (screenHeight - obj.height()) / 2 + scrolltop;
+                obj.css({left: objLeft + 'px', top: objTop + 'px'});
+            });
+
+            $(window).scroll(function () { //有滚动条时计算位置
+                screenWidth = $(window).width();
+                screenHeight = $(window).height();
+                scrolltop = $(document).scrollTop();
+                objLeft = (screenWidth - obj.width()) / 2;
+                objTop = (screenHeight - obj.height()) / 2
+                obj.css({left: objLeft + 'px', top: objTop + 'px'});
+            });
         }
     }
 })();
--- confirm/confirm.tpl
+++ confirm/confirm.tpl
@@ -1,5 +1,16 @@
-<div>
-    <h3>提示：</h3>
-    <span id="confirm-message"></span>
-    <button id="confirm-btn">确定</button>
+<div class="sys-div-confirm">
+    <div class="confirm-content">
+        <div class="confirm-header clearfix">
+            <h3><i class="confirm-alert"><!-- 警告 --></i>提示</h3>
+            <i class="confirm-close">&times;</i>
+        </div>
+        <div class="confirm-body">
+            <span id="confirm-message"></span>
+        </div>
+        <div class="confirm-footer clearfix">
+            <button class="confirm-dismiss">关闭</button>
+            <button class="confirm-sure" id="confirm-btn">确定</button>
+        </div>
+    </div>
 </div>
+<div class="confirm-mask"><!-- 遮罩 --></div>
