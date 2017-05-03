f884ee1c5cd98386c1a67205b922c76898943a27
Small <a364717646@163.com>
2017-05-03 14:23:36 +0800

    add phone

--- valid.js
+++ valid.js
@@ -12,7 +12,13 @@ $(function() {
         var elements=this[0].elements;
         for (var index=0,size=elements.length;index<size;index++) {
             var item=$(elements[index]);
-            var val=item.val();
+            var val=item.val(),t;
+            if (t=item.data('repeat')){
+                if (item.val()!=elements[t].value){
+                    error(item[0],'请确认信息一致');
+                    return false;
+                }
+            }
             if (item.attr('required')){
                 if (val==''||(val=='0'&&item[0].type=='select-one')){
                     error(item[0],'请完善信息');
@@ -20,11 +26,16 @@ $(function() {
                 }
             }
             if (val=='') continue;
-            var t;
-            if (t=item.data('repeat')){
-                if (item.val()!=elements[t].value){
-                    error(item[0],'请确认信息一致');
-                    return false;
+            if (t=item.data('type')){
+                switch (t) {
+                    case 'phone':
+                        if (isNaN(val)||val.length!=11){
+                            error(item[0],'请确认手机号码是否正确');
+                            return false;
+                        }
+                        break;
+                    default:
+                        break;
                 }
             }
         }
